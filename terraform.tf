b73b981af6eff0edfcc5bb155225dac833dac90f
dejano <dejano.with.tie@gmail.com>
2023-01-14 15:05:51 +0100

    initial

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "magicskunk"
+
+    workspaces {
+      name = "seventh-dev"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.50.0"
+    }
+  }
+
+  required_version = ">= 1.4.0"
+}
