68439dd4a4f0d924a698518033811862e8ed460b
dejano <dejano.with.tie@gmail.com>
2023-01-14 17:56:28 +0100

    Provide generic input var `tags`

--- modules/container-repository/variables.tf
+++ modules/container-repository/variables.tf
@@ -6,6 +6,12 @@ variable "project_name" {
   type    = string
 }
 
+variable "tags" {
+  description = "Map of tags to set."
+  type        = map(string)
+  default     = {}
+}
+
 variable "container_repository_name" {
   type = string
   description = "Name of the container repository"
