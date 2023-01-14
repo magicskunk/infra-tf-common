fb8943ba8f3f3320155ffe5ebbd34443053e8e7b
dejano <dejano.with.tie@gmail.com>
2023-01-14 20:18:11 +0100

    Define pull & push policies for main ecr

--- modules/container-repository/main.tf
+++ modules/container-repository/main.tf
@@ -10,3 +10,50 @@ resource "aws_ecr_repository" "main" {
     Name = "ecr"
   }
 }
+
+# do I need this env based?
+resource "aws_iam_policy" "aws_ecr_repository_main_pull_allowed_policy" {
+  name        = "ecr-pull-allowed-policy"
+  description = "Allow pull from ECR policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowPull",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ],
+        "Resource" : aws_ecr_repository.main.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "aws_ecr_repository_main_pull_push_allowed_policy" {
+  name        = "ecr-pull-push-allowed-policy"
+  description = "Allow pull & push to ecr policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowPullPush",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ],
+        "Resource" : aws_ecr_repository.main.arn
+      }
+    ]
+  })
+}
