c6263651e4cc6b122f85af90a1dd7bcf9e04b8bb
dejano <dejano.with.tie@gmail.com>
2023-01-28 17:10:41 +0100

    Cleanup

--- ses.tf
+++ ses.tf
@@ -25,7 +25,7 @@ resource "aws_ses_domain_dkim" "ses_domain_dkim" {
   domain = join("", aws_ses_domain_identity.ses_domain.*.domain)
 }
 
-resource "aws_route53_record" "amazonses_dkim_record" {
+resource "aws_route53_record" "ses_dkim_record" {
   count   = 3
   zone_id = data.aws_route53_zone.primary.zone_id
   name    = "${element(aws_ses_domain_dkim.ses_domain_dkim.dkim_tokens, count.index)}._domainkey.${var.email_from_domain}"
